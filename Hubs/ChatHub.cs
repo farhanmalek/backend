98663d432a8139eb8285eec849567615051df6b8
Farhan Malek <malekf2328@gmail.com>
2024-06-01 01:24:31 +1200

    working ws

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,8 +1,6 @@
-// Purpose: Contains the ChatHub class which is used to handle SignalR connections for the chat feature of the application.
 using backend.Dtos.Account;
 using backend.Dtos.Message;
 using backend.Interfaces;
-
 using Microsoft.AspNetCore.SignalR;
 
 namespace backend.Hubs
@@ -10,9 +8,21 @@ namespace backend.Hubs
     public class ChatHub : Hub
     {
         private readonly IMessageService _messageService;
+        
         public ChatHub(IMessageService messageService)
         {
             _messageService = messageService;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            // Assuming the chatId is passed as a query string parameter
+            var chatId = Context.GetHttpContext().Request.Query["chatId"];
+            if (!string.IsNullOrEmpty(chatId))
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, chatId);
+                await base.OnConnectedAsync();
+            }
         }
 
         public async Task SendMessageByChatId(GetUserDto user, GetMessageDtoFromClient message, int chatId)
@@ -20,6 +30,5 @@ namespace backend.Hubs
             await _messageService.SaveMessage(chatId, user.UserId, message.Content!);
             await Clients.Group(chatId.ToString()).SendAsync("ReceiveMessageByChatId", user, message);
         }
-      
     }
 }
